62327b8782af1284428e37733447e2fbfe89dfd4
nicholaskier <nicholaskier@gmail.com>
2021-03-02 10:17:31 -0600

    add if else

--- app1.py
+++ app1.py
@@ -3,7 +3,11 @@ import json
 data = json.load(open("data.json"))
 
 def define(word):
-    return data[word]
+    if word in data:
+        return data[word]
+    else:
+        return "The word does not exist. Please double check it."
+
 
 word = input("Enter word:")
 
