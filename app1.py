344b82d88ea4c224b512c220f620351d6c123f02
nicholaskier <nicholaskier@gmail.com>
2021-03-02 15:25:56 -0600

    add acronyms and proper nouns

--- app1.py
+++ app1.py
@@ -7,6 +7,10 @@ def define(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     elif len(get_close_matches(word, data.keys())) > 0:
         confirm = input("Did you mean %s instead? Enter Y if yes, or N if no." % get_close_matches(word, data.keys())[0])
         if confirm == "Y":
